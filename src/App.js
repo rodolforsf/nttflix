3d834a88ed2e99083a3b562f55ca67a2bb9d577b
Rodolfo Faria <106396728+rodolforsf@users.noreply.github.com>
2024-02-03 21:37:14 -0300

    Criando Row

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import './App.css';
+import Row from './components/Row';
+import categories from './api';
 
 function App() {
   return (
     <div className="App">
-      
+      {categories.map((category) => {
+        return (
+          <Row 
+            key={category.name} 
+            title={category.title} 
+            path={category.path} 
+            isLarge={category.isLarge}
+          />
+        );
+      })}
     </div>
   );
 }
--- src/api.js
+++ src/api.js
@@ -4,51 +4,61 @@ const categories = [
    { 
         name: "trending",
         title: "Em alta",
-        path: `/treding/all/week?api_key=${API_KEY}&language=pt_BR`,
+        path: `/trending/all/week?language=pt-BR&api_key=${API_KEY}`,
+        isLarge: true,
     },
    { 
         name: "netflixOriginals",
         title: "Originais Netflix",
-        path: `discover/tv?api_key=${API_KEY}&language=pt_BR&with_networks=213`,
+        path: `/discover/tv?api_key=${API_KEY}&with_networks=213&language=pt-BR`,
+        isLarge: false,
     },
    { 
         name: "topRated",
         title: "Populares",
         path: `/movie/top_rated?api_key=${API_KEY}&language=pt_BR`,
+        isLarge: false,
     },
    { 
         name: "animation",
         title: "Animação",
         path: `/discover/tv?api_key=${API_KEY}&with_genres=16&language=pt-BR`,
+        isLarge: false,
     },
    { 
         name: "comedy",
         title: "Comédia",
         path: `/discover/tv?api_key=${API_KEY}&with_genres=35&language=pt-BR`,
+        isLarge: false,
     },
    { 
         name: "drama",
         title: "Drama",
         path: `/discover/tv?api_key=${API_KEY}&with_genres=18&language=pt-BR`,
+        isLarge: false,
     },
    { 
         name: "western",
         title: "Faroeste",
         path: `/discover/tv?api_key=${API_KEY}&with_genres=37&language=pt-BR}R`,
+        isLarge: false,
     },
    { 
         name: "romance",
         title: "Romance",
         path: `/discover/tv?api_key=${API_KEY}&with_genres=10749&language=pt-BR`,
+        isLarge: false,
     },
 ];
 
 export const getMovies = async (path) => {
     try {
-        let url = `https://api.themoviedb.org/3/${path}`;
+        let url = `https://api.themoviedb.org/3${path}`;
         const response = await fetch(url);
         return await response.json();
     } catch (error) {
         console.log("error getMovies: ", error);
     }
 };
+
+export default categories;
